a35679ef00cfbbe92e3dc4c794b29ea6552d8d28
amandeepta <amandiptaitis@gmail.com>
2025-05-14 10:17:21 +0530

    Added the styling

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include "MyWindow.h"
+#include <gtkmm/cssprovider.h>
+#include <gtkmm/stylecontext.h>
+#include <gdkmm/display.h>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    auto app = Gtk::Application::create("org.gtkmm.examples.navigation");
+    auto app = Gtk::Application::create("org.gtkmm.example.cliapp");
+
+    auto css_provider = Gtk::CssProvider::create();
+    try {
+        css_provider->load_from_path("style.css");
+        Gtk::StyleContext::add_provider_for_display(
+            Gdk::Display::get_default(),
+            css_provider,
+            GTK_STYLE_PROVIDER_PRIORITY_USER
+        );
+    } catch (const Glib::Error& ex) {
+        std::cerr << "Failed to load CSS: " << ex.what() << std::endl;
+    }
+
     return app->make_window_and_run<MyWindow>(argc, argv);
 }
